048efd5e04789b9ee1a9edfef714e5058bae4333
tusshigoyal <56935385+tusshigoyal@users.noreply.github.com>
2019-10-24 00:17:28 +0530

    update in exception

--- fileexception.py
+++ fileexception.py
@@ -7,6 +7,15 @@ try:
 	finally:
 		filptr.close()
 		print("File is closed buddy")
+		while True:
+    x.append(i)
+    y.append(psutil.cpu_percent())
+    print(len(y))
+    if len(y) >=20:
+        y.pop(0)
+        x.pop(0)
+    ax.plot(x, y, color='b')  
+    fig.canvas.draw()
 
 except:
 	print("error generated bbye")
